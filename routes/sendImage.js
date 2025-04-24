1d134a6b30b2a4d7205f06954590da284e41077f
Sławek <slawekzdunek2.3.6@gmail.com>
2025-04-24 16:25:11 +0200

    Adoption listings for shelters

--- routes/sendImage.js
+++ routes/sendImage.js
@@ -8,48 +8,48 @@ const router = express.Router();
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/sendImage", upload.single("image"), async (req, res) => {
-    if (!req.file)
-        res.statusMessage(StatusCodes.BAD_REQUEST).send("No file uploaded.");
+router.post("", upload.single("image"), async (req, res) => {
+	if (!req.file) res.status(StatusCodes.BAD_REQUEST).send("No file uploaded.");
 
-    const { originalname, encoding, mimetype, size, buffer } = req.file;
+	const { originalname, encoding, mimetype, size, buffer } = req.file;
 
-    try {
-        await db.execute(
-            `
-            INSERT INTO images (Name, Encoding, Mimetype, Size, ImageData)
+	try {
+		const result = await db.execute(
+			`
+            INSERT INTO images (name, encoding, mimetype, size, data)
             VALUES (?, ?, ?, ?, ?)
             `,
-            [originalname, encoding, mimetype, size, buffer]
-        );
+			[originalname, encoding, mimetype, size, buffer],
+		);
 
-        return res.status(StatusCodes.CREATED).json({
-            message: "Image uploaded successfully",
-        });
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
-    }
+		console.log(result);
+		return res.status(StatusCodes.CREATED).json({
+			message: "Image uploaded successfully",
+		});
+	} catch (error) {
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+	}
 });
 
-router.get("/getImage/:fileName", async (req, res) => {
-    const { fileName } = req.params;
+router.get("/:id", async (req, res) => {
+	const { id } = req.params;
 
-    try {
-        const [rows] = await db.execute(
-            `
-            SELECT * FROM images WHERE images.name = ?
+	try {
+		const [rows] = await db.execute(
+			`
+            SELECT name,encoding, mimetype,size, data FROM images WHERE images.id_image = ?
             `,
-            [fileName]
-        );
+			[id],
+		);
 
-        const file = rows[0];
-        console.log(file);
+		const file = rows[0];
+		console.log(file);
 
-        res.setHeader("Content-Type", file.Mimetype);
-        res.setHeader("Content-Disposition", 'inline; filename="${file.Name}"');
-        return res.status(StatusCodes.CREATED).send(file.ImageData);
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
-    }
+		res.setHeader("Content-Type", file.mimetype);
+		res.setHeader("Content-Disposition", 'inline; filename="${file.name}"');
+		return res.status(StatusCodes.CREATED).send(file.data);
+	} catch (error) {
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+	}
 });
 module.exports = router;
--- routes/shelter.js
+++ routes/shelter.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const { authenticate } = require("../middleware/authenticate.js");
 const { validateLogin } = require("../middleware/validate.js");
-const { login, info } = require("../controllers/shelterController.js");
+const { login, info, listing } = require("../controllers/shelterController.js");
 
 router.post("/login", validateLogin, login).get("/info", authenticate, info);
+router.post("/listing", listing);
 
 module.exports = router;
