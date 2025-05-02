a46ffcdeeb8002c4c1a5a012e3da67bcca3a5897
Sławek <slawekzdunek2.3.6@gmail.com>
2025-05-02 15:32:12 +0200

    get listings endpoint

--- routes/shelter.js
+++ routes/shelter.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 const { authenticate } = require("../middleware/authenticate.js");
 const { validateLogin } = require("../middleware/validate.js");
-const { login, info, listing } = require("../controllers/shelterController.js");
+const {
+	login,
+	info,
+	listing,
+	listings,
+} = require("../controllers/shelterController.js");
 
 router.post("/login", validateLogin, login).get("/info", authenticate, info);
-router.post("/listing", listing);
+router.post("/listing", listing).get("/listings", listings);
 
 module.exports = router;
