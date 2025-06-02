c440a0cfe2419610eef674c42755d69245b04c8c
Jakub Kołdarz <jakub213x@gmail.com>
2025-06-02 20:53:16 +0200

    Added Tags GET

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -15,36 +15,38 @@ const addTag = async (req, res) => {
     try {
         const newTag = await TagService.createTag(tagData);
 
-        return res
-            .status(StatusCodes.CREATED)
-            .json({ message: "Tag created successfully", tag: newTag });
+        return res.status(StatusCodes.CREATED).json({ message: "Tag created successfully", tag: newTag });
     } catch (error) {
-        return res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ message: error.message });
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
     }
 };
 
 const updateTag = async (req, res) => {
     const tagData = {
         id_tag: req.params.id,
-        character: req.body.character
+        character: req.body.character,
     };
 
     try {
         const newTag = await TagService.updateTag(tagData.id_tag, tagData.character);
 
-        return res
-            .status(StatusCodes.CREATED)
-            .json({ message: "Tag updated successfully", tag: newTag });
+        return res.status(StatusCodes.CREATED).json({ message: "Tag updated successfully", tag: newTag });
     } catch (error) {
-        return res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ message: error.message });
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+    }
+};
+
+const getTags = async (req, res) => {
+    try {
+        const tags = await TagService.getAllTags();
+        return res.json({ tags: tags });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
     }
 };
 
 module.exports = {
     addTag,
-    updateTag
+    updateTag,
+    getTags,
 };
--- routes/shelter.js
+++ routes/shelter.js
@@ -1,27 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-    authenticate,
-    authorizeRole,
-} = require("../middleware/authenticate.js");
+const { authenticate, authorizeRole } = require("../middleware/authenticate.js");
 const { validate } = require("../middleware/validate.js");
-const {
-    addShelter,
-    updateShelter,
-    allShelters,
-    assignUserToShelter,
-} = require("../controllers/shelterController.js");
+const { addShelter, updateShelter, allShelters, assignUserToShelter } = require("../controllers/shelterController.js");
 
 router
-    .post(
-        "/",
-        authenticate,
-        authorizeRole("admin"),
-        validate("shelter"),
-        addShelter
-    )
-    .put("/:id", authenticate, authorizeRole("admin"), updateShelter)
+    .post("/", authenticate, authorizeRole("admin"), validate("shelter"), addShelter)
+    .put("/:id", authenticate, authorizeRole("admin"), validate("shelter"), updateShelter)
     .get("/", authenticate, authorizeRole("admin"), allShelters)
     .post("/assign", authenticate, authorizeRole("admin"), assignUserToShelter);
 
