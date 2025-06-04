938b74e005df9178cb1bbb3619205fb9e5640732
Bartosz Kawalec <66064613+Bulb1@users.noreply.github.com>
2025-06-04 21:03:46 +0200

    Update advice.js

--- routes/advice.js
+++ routes/advice.js
@@ -6,6 +6,8 @@ const { authenticate, authorizeRole } = require("../middleware/authenticate");
 const { validate } = require("../middleware/validate.js");
 
 router
+    .get("/", authenticate, authorizeRole("shelter"), getAdvices)
+    .get("/:id", authenticate, authorizeRole("shelter"), getAdvice)
     .post("/", authenticate, authorizeRole("shelter"),  validate("advice"),  addAdvice)
     .put("/:id", authenticate, authorizeRole("shelter"), updateAdvice)
 
