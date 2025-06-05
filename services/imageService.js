3416fad67b15d57a2dd7e50e696083f3752edbe9
Jakub Kołdarz <jakub213x@gmail.com>
2025-06-06 00:19:12 +0200

    DB Fix

--- services/imageService.js
+++ services/imageService.js
@@ -1,19 +1,17 @@
-const pool = require("../db");
-
 async function createImage({ name, encoding, mimetype, size, data }) {
-	const query = `
+    const query = `
     INSERT INTO images (name, encoding, mimetype, size, data)
     VALUES ($1, $2, $3, $4, $5)
     RETURNING id_image;
   `;
-	const values = [name, encoding, mimetype, size, data];
-	const result = await pool.query(query, values);
+    const values = [name, encoding, mimetype, size, data];
+    const result = await pool.query(query, values);
 
-	return { id_image: result.rows[0].id_image };
+    return { id_image: result.rows[0].id_image };
 }
 
 async function changeImageData(id_image, { name, encoding, mimetype, size, data }) {
-	const query = `
+    const query = `
     UPDATE images
     SET name = $1,
         encoding = $2,
@@ -23,32 +21,32 @@ async function changeImageData(id_image, { name, encoding, mimetype, size, data 
         created_at = NOW()
     WHERE id_image = $6;
   `;
-	const values = [name, encoding, mimetype, size, data, id_image];
-	await pool.query(query, values);
+    const values = [name, encoding, mimetype, size, data, id_image];
+    await pool.query(query, values);
 }
 
 async function getImageByID(id_image) {
-	const query = `
+    const query = `
     SELECT name, mimetype, data
     FROM images
     WHERE id_image = $1;
   `;
-	const values = [id_image];
-	const result = await pool.query(query, values);
+    const values = [id_image];
+    const result = await pool.query(query, values);
 
-	if (result.rows.length === 0) {
-		return null;
-	}
+    if (result.rows.length === 0) {
+        return null;
+    }
 
-	return {
-		name: result.rows[0].name,
-		mimetype: result.rows[0].mimetype,
-		data: result.rows[0].data,
-	};
+    return {
+        name: result.rows[0].name,
+        mimetype: result.rows[0].mimetype,
+        data: result.rows[0].data,
+    };
 }
 
 module.exports = {
-	createImage,
-	changeImageData,
-	getImageByID,
+    createImage,
+    changeImageData,
+    getImageByID,
 };
