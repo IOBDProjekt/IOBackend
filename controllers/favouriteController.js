c713a781b5cafb13027442edf3c3f0da74ca75a3
DarkSourcerer-SWMG <158461898+DarkSourcerer-SWMG@users.noreply.github.com>
2025-06-10 09:54:11 +0200

    changed favourites controller return

--- controllers/favouriteController.js
+++ controllers/favouriteController.js
@@ -6,66 +6,74 @@ const { StatusCodes } = require("http-status-codes");
 const FavouriteService = require("../services/favouriteService");
 
 const newFavourite = async (req, res) => {
-    const petID = req.body["id_pet"];
-    const userID = req.authData["id_user"];
+  const petID = req.body["id_pet"];
+  const userID = req.authData["id_user"];
 
-    try {
-        const fav = await FavouriteService.createFavourite(userID, petID);
+  try {
+    const fav = await FavouriteService.createFavourite(userID, petID);
 
-        return res.status(StatusCodes.CREATED).json({
-            message: "Successfuly added pet to your favourites list",
-            favourite: fav,
-        });
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
-    }
+    return res.status(StatusCodes.CREATED).json({
+      message: "Successfuly added pet to your favourites list",
+      favourite: fav,
+    });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
 };
 
 const removeFavourite = async (req, res) => {
-    const petID = req.params.id;
-    const userID = req.authData["id_user"];
+  const petID = req.params.id;
+  const userID = req.authData["id_user"];
 
-    try {
-        const result = await FavouriteService.deleteFavourite(userID, petID);
+  try {
+    const result = await FavouriteService.deleteFavourite(userID, petID);
 
-        return res.status(StatusCodes.OK).json({ message: "Pomyślnie usunięto polubienie" });
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
-    }
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: "Pomyślnie usunięto polubienie" });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
 };
 
 const allFavourite = async (req, res) => {
-    const userID = req.authData["id_user"];
+  const userID = req.authData["id_user"];
 
-    try {
-        const favs = await FavouriteService.getFavouritesByUserID(userID);
+  try {
+    const favs = await FavouriteService.getFavouritesByUserID(userID);
 
-        return res.json({
-            favourites: favs,
-        });
-    } catch (error) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            message: error.message,
-        });
-    }
+    return res.json({
+      favourites: favs.map((fav) => fav.pet),
+    });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: error.message,
+    });
+  }
 };
 
 const checkFavourite = async (req, res) => {
-    const userID = req.authData["id_user"];
-    const petID = req.body["id_pet"];
+  const userID = req.authData["id_user"];
+  const petID = req.body["id_pet"];
 
-    try {
-        const result = await FavouriteService.checkFavouriteByUserID(userID, petID);
+  try {
+    const result = await FavouriteService.checkFavouriteByUserID(userID, petID);
 
-        return res.json({ result: result });
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
-    }
+    return res.json({ result: result });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
 };
 
 module.exports = {
-    newFavourite,
-    removeFavourite,
-    allFavourite,
-    checkFavourite,
+  newFavourite,
+  removeFavourite,
+  allFavourite,
+  checkFavourite,
 };
